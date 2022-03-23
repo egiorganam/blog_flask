b37dbc0704912ab1901b2ef68062768d76f6a085
Mona Lisa <giorgana.elih@gmail.com>
2022-03-22 22:48:23 -0600

    connection to db, and forms configured with the db

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
+from flask import url_for
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from slugify import slugify
+from sqlalchemy.exc import IntegrityError
+from run import db
 
 
 class User(db.Model, UserMixin):
-
     __tablename__ = 'blog_user'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -23,6 +25,7 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password, password)
 
     """METODO PARA GUARDAR USUARIOS EN LA BASE DE DATOS"""
+
     def save(self):
         if not self.id:
             db.session.add(self)
@@ -36,3 +39,40 @@ class User(db.Model, UserMixin):
     def get_by_email(email):
         return User.query.filter_by(email=email).first()
 
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('blog_user.id', ondelete='CASCADE'), nullable=False)
+    title = db.Column(db.String(256), nullable=False)
+    title_slug = db.Column(db.String(256), unique=True, nullable=False)
+    content = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
+
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        if not self.title_slug:
+            self.title_slug = slugify(self.title)
+
+        saved = False
+        count = 0
+        while not saved:
+            try:
+                db.session.commit()
+                saved = True
+            except IntegrityError:
+                count += 1
+                self.title_slug = f'{slugify(self.title)}-{count}'
+
+    def public_url(self):
+        return url_for('show_post', slug=self.title_slug)
+
+    @staticmethod
+    def get_by_slug(slug):
+        return Post.query.filter_by(title_slug=slug).first()
+
+    @staticmethod
+    def get_all():
+        return Post.query.all()
